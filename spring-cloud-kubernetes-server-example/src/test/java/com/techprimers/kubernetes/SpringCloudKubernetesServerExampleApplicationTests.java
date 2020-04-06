3a70b65afc9770e63f0917c0a02a3be11f9f0a86
ashoakskumar <34050243+ashoakskumar@users.noreply.github.com>
2020-04-06 19:37:52 +0530

    server added

---
+++ spring-cloud-kubernetes-server-example/src/test/java/com/techprimers/kubernetes/SpringCloudKubernetesServerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techprimers.kubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudKubernetesServerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
